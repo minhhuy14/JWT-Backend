8de7d00fafc51200f3369eb8c04ebbec5921889a
minhhuy14 <minhhuy14012003@gmail.com>
2024-03-17 00:02:24 +0700

    Complete display list users

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,20 +1,16 @@
-// Get the client
-const mysql = require('mysql2')
 
-// Create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt',
-});
+import userService from '../service/userService.js'
 
+
+userService.initializeConnection();
 
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs");
 }
 
-const handleUserPage = (req, res) => {
-    return res.render("user.ejs")
+const handleUserPage = async (req, res) => {
+    let listUsers = await userService.getAllUsers();
+    return res.render("user.ejs", { listUsers })
 }
 
 const handleCreateNewUser = (req, res) => {
@@ -22,15 +18,22 @@ const handleCreateNewUser = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let username = req.body.username;
-    try {
-        const result = connection.query(
-            'INSERT INTO Users(email, password,username) values (?,?,?)', [email, password, username]
-        );
-        console.log(result);
 
-    } catch (err) {
-        console.log(err);
-    }
+    // let hashPassword = bcrypt.hashSync(password, salt);
+    // console.log(hashPassword);
+    // try {
+    //     const result = connection.query(
+    //         'INSERT INTO Users(email, password,username) values (?,?,?)', [email, password, username]
+    //     );
+    //     // console.log(result);
+
+    //     let check = bcrypt.compareSync(password, hashPassword);
+    //     console.log('Check pass ', check);
+
+    // } catch (err) {
+    //     console.log(err);
+    // }
+    userService.createNewUser(email, password, username);
     return res.send("Handle new user");
 }
 module.exports = {
