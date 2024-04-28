a63595d90eb6b303e9674be6badebfc98e70bbcb
minhhuy14 <minhhuy14012003@gmail.com>
2024-04-28 16:39:38 +0700

    write test api

--- routes/web.js
+++ routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
 const router = express.Router();
 
 /**
@@ -9,9 +10,11 @@ const router = express.Router();
 const initWebRouters = (app) => {
     router.get("/", homeController.handleHelloWorld);
     router.get("/user", homeController.handleUserPage);
-    router.post("/users/create-user", homeController.handleCreateNewUser)
-    router.post("/users/delete-user/", homeController.handleDeleteUser)
-    router.post("/users/edit-user/", homeController.handleEditUser)
+    router.post("/users/create-user", homeController.handleCreateNewUser);
+    router.post("/users/delete-user/", homeController.handleDeleteUser);
+    router.post("/users/edit-user/", homeController.handleEditUser);
+
+    router.get("/api/test-api", apiController.testApi)
     return app.use("/", router);
 }
 
